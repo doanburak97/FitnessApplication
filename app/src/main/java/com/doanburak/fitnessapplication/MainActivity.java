5f4e350c249f9a107b35fa1215286370fc022489
doanb <doanburak97@gmail.com>
2021-01-28 19:41:56 +0300

    second commit

--- app/src/main/java/com/doanburak/fitnessapplication/MainActivity.java
+++ app/src/main/java/com/doanburak/fitnessapplication/MainActivity.java
@@ -1,14 +1,47 @@
 package com.doanburak.fitnessapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.options_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.logout){
+            mAuth.signOut();
+            Intent toLogin = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(toLogin);
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
